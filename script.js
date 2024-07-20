c95e7004b32962e2caf0b3e3df279e5226b2aacc
Dipwr <deopwr@gmail.com>
2024-07-21 00:50:30 +0200

    Last modified updated with Github API

--- script.js
+++ script.js
@@ -8,10 +8,6 @@ const images = [
     "media/slash/frame_6_delay-0.13s.png"
 ];
 
-function setup(){
-    document.getElementById("modified").innerHTML = "Last modified: Thu Jul 19 16:29:26 2024";
-}
-
 //on click animation code found on: https://stackoverflow.com/questions/4847996/css-animation-onclick
 function titleAni() {
     let element = document.getElementById("titleImage")
@@ -50,4 +46,60 @@ function buttonClick(href) {
     snd2.play();
 }
 
-setup();
+function convertMonth(month){
+    switch (month) {
+        case '01':
+            return "Jan";
+        case '02':
+            return "Feb";
+        case '03':
+            return "Mar";
+        case '04':
+            return "Apr";
+        case '05':
+            return "May";
+        case '06':
+            return "June";
+        case '07':
+            return "July";
+        case '08':
+            return "Aug";
+        case '09':
+            return "Sept";
+        case '10':
+            return "Oct";
+        case '11':
+            return "Nov";
+        case '12':
+            return "Dec";
+    }
+}
+
+function convertDate(date) {
+    let year = date.slice(0,4);
+    let month = convertMonth(date.slice(5,7));
+    let day = date.slice(8,10)
+    let time = date.slice(11,19)
+
+    return month + ' ' + day + ' ' + time + " (UCT) " + year;
+}
+
+//api request using fetch from: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+async function getData() {
+    const url = "https://api.github.com/repos/Dipwr/sans/commits/main";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      document.getElementById("modified").innerHTML = "Last modified: " + convertDate(json.commit.author.date);
+    } catch (error) {
+      console.error(error.message);
+    }
+
+    
+}
+
+getData()
